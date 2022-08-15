dc76a222718925d70ff0be0627f9f8a1fc37257f
Daniel Diaz Santiago <daniel.diaz.stg@gmail.com>
2022-08-14 20:42:32 -0600

    author

--- stock_menu_v3.py
+++ stock_menu_v3.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Aug 30 22:57:11 2021
+Created on Sun Aug 14 2022
 
-@author: D99003734
+@author: D41147599
 """
 from datetime import datetime
 from stock_class import Stock, DailyData
